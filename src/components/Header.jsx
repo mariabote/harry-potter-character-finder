27b6a750627e996489f2dae8b516b0dda1c7f2ca
Maria Bote <enbotedebote@gmail.com>
2024-04-09 12:08:11 +0200

    Github pages routes images changed in docs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     <header>
       <img
         className="logo__hp"
-        src="../docs/images/harry-potter-logo-32524.png"
+        src="./images/harry-potter-logo-32524.png"
         alt="logo con el texto Harry Potter"
       />
     </header>
